d00a2231f9f7150acfe271fd8a2d44e4d9d76d45
mriverov3@gmail.com <mriverov3@gmail.com@274eac56-1ffe-c277-2579-567203511452>
2011-12-13 03:59:05 +0000

    Creacion de clases y metodos

--- trunk/CruceDeSemaforos/src/model/Cruce.java
+++ trunk/CruceDeSemaforos/src/model/Cruce.java
@@ -1,5 +1,22 @@
 package model;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Cruce {
+	
+	private Lock semaforoEsquinaNE = new ReentrantLock();
+	private Lock semaforoEsquinaSE = new ReentrantLock();
+	private Lock semaforoEsquinaSO = new ReentrantLock();
+	private Avenida calleOeste = new Avenida();
+	private Avenida calleSur = new Avenida();
+	private Avenida calleEste = new Avenida();
+	
+	
+	
+	
+	
+	public void inicializarCruce(){}
+	
 
 }
--- trunk/CruceDeSemaforos/src/model/Semaforo.java
+++ trunk/CruceDeSemaforos/src/model/Semaforo.java
@@ -1,5 +1,22 @@
 package model;
 
+import java.util.concurrent.locks.Lock;
+
 public class Semaforo extends Thread {
+	
+	private String color;
+	private Lock turno;
+	
+	
+	
+	public Semaforo(String color, Lock turno) {
+		
+		this.color = color;
+		this.turno = turno;
+	}
+
+
+
+	public void run(){}
 
 }
