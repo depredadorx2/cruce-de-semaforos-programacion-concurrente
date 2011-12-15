26cc625a9dc4f82bf07e9778b074afa1ae0f6fad
mriverov3@gmail.com <mriverov3@gmail.com@274eac56-1ffe-c277-2579-567203511452>
2011-12-15 03:59:41 +0000

    Version jueves 1 am. Falta solo un dia, tiene deadlock en alguna parte.

--- trunk/CruceDeSemaforos/src/model/AutoCruceDoble.java
+++ trunk/CruceDeSemaforos/src/model/AutoCruceDoble.java
@@ -4,15 +4,50 @@ import java.util.concurrent.Semaphore;
 
 public class AutoCruceDoble extends Auto {
 	
-	private Semaphore semaforoPaso2;
+	public Semaphore semaforoPaso2;
+	public String tipoDeCruce2;
 
-	public AutoCruceDoble(int velocidad, Avenida calle) {
-		super(velocidad, calle);
-		this.semaforoPaso2= new Semaphore(1);
+	public AutoCruceDoble(int velocidad, Avenida calle, Semaphore semaforoCubito, String modelo, int matricula,String tipoDeCruce , Semaphore semaforoCubito2, String tipoDeCruce2) {
+		super(velocidad,  calle,  semaforoCubito,  modelo,  matricula, tipoDeCruce);
+		this.semaforoPaso2 = semaforoCubito2;
+		this.tipoDeCruce2 = tipoDeCruce2;
+		
 		
 	}
 	
 	@Override
-	public void start(){}
+	public void start(){
+		try {
+			this.unirmeAlTransito();
+			System.out.println(super.modeloAuto+" "+super.matricula +"  estoy en transito, espero semaforo verde "+ " calle " + super.calle.nombre);
+			
+				wait(); //espero que al semaforo
+				
+				System.out.println(super.modeloAuto+" "+super.matricula +"  cruce semaforo " + " calle " + super.calle.nombre);
+				
+				super.semaforoPaso1.acquireUninterruptibly();
+				
+				System.out.println(super.modeloAuto+" "+super.matricula +"  ocupe " + super.tipoDeCruce);
+				Thread.sleep(super.velocidad);
+				super.semaforoPaso1.release();
+				
+				System.out.println(super.modeloAuto+" "+super.matricula +"  desocupe " + super.tipoDeCruce);
+				
+				this.semaforoPaso2.acquireUninterruptibly();
+				System.out.println(super.modeloAuto+" "+super.matricula +"  ocupe " + this.tipoDeCruce2);
+				Thread.sleep(super.velocidad);
+				this.semaforoPaso2.release();
+				System.out.println(super.modeloAuto+" "+super.matricula +"  desocupe " + this.tipoDeCruce2);
+				
+				
+				
+			} catch (Exception e) {}
+		
+	}
+	
+	public synchronized void curzar(){
+		notify(); // esto lo hace fair? o sea, libera al auto que realmente esta primero?
+	}
+	
 
 }
--- trunk/CruceDeSemaforos/src/model/Semaforo.java
+++ trunk/CruceDeSemaforos/src/model/Semaforo.java
@@ -1,22 +1,101 @@
 package model;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Semaforo extends Thread {
 	
-	private String color;
-	private Lock turno;
+	public String color;
+	public String indentificacion;
+	private Semaphore semaforoVerde;
+	private Semaphore semaforoRojo;
+	public static  Avenida calle;
+	public Semaphore mutexBoleano = new Semaphore(1);
+	public static Lock l = new ReentrantLock();
+	public static Condition esperarVerde = l.newCondition();
+	public static Boolean estaEnVerde = true;
+	public SemaforoLiberadorDeAutos sema = new SemaforoLiberadorDeAutos();
 	
-	
-	
-	public Semaforo(String color, Lock turno) {
+	/**
+	 * Constructor de un semafror de esquina.
+	 * @param semaforoVerde
+	 * @param semaforoRojo
+	 * @param calle
+	 */
+	public Semaforo(Semaphore semaforoVerde, Semaphore semaforoRojo, Avenida calle, String identificacion) {
 		
-		this.color = color;
-		this.turno = turno;
+		
+		this.semaforoVerde = semaforoVerde;
+		this.semaforoRojo=semaforoRojo;
+		Semaforo.calle=calle;
+		this.indentificacion = identificacion;
+		
+		
+		
 	}
 
+/**
+ * Primero pide turno para ser verde, luego libera al primer auto y espera para ser amarillo y despues rojo, para ser rojo tambien pide permiso 
+ * para tomar uno de los semaforos
+ */
 
-
-	public void run(){}
-
+	public void run(){
+		sema.start();
+		
+		try{
+			while (true){
+				System.out.println("espero a ser verde "+ this.indentificacion);
+		this.semaforoVerde.acquireUninterruptibly(); //pido para ser verde
+		
+		System.out.println("soy verde "+ this.indentificacion);
+		mutexBoleano.acquireUninterruptibly(); //protejo el boleano
+		
+		estaEnVerde=true;
+		
+		esperarVerde.signal(); //libero mi thread liberador de autos
+		mutexBoleano.release();  
+		
+		
+		Thread.sleep(2000);
+		
+		this.color = "Amarillo";
+		
+		Thread.sleep(400);
+		
+		mutexBoleano.acquireUninterruptibly(); //cambio el booleano para que dejen de pasar autos
+		estaEnVerde=false;
+		mutexBoleano.release();
+		
+		this.semaforoVerde.release(); //libero el semaforo verde
+		this.semaforoRojo.acquireUninterruptibly(); //ahora soy rojo
+			}
+		}
+		catch(Exception e){}
+		
+		
+	}
+	
+	public static class SemaforoLiberadorDeAutos extends Thread{
+		
+		public void run(){
+			
+			l.lock();
+			try{
+			while(true){
+				while(estaEnVerde){
+					calle.puedenCruzar(); // este esta adefinido en calle, lo que hace es a su primer auto lo libera
+				}
+				
+				esperarVerde.await();
+				
+			}
+			
+		}
+		
+		catch(Exception e){}
+	  l.unlock();
+		} 
+}
 }
