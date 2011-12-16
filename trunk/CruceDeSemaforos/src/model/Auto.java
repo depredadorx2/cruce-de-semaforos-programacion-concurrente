be0bce340ba39286ae191c55fc12f82afe33b012
gleo84 <gleo84@274eac56-1ffe-c277-2579-567203511452>
2011-12-16 07:43:28 +0000

    esperemos que si!

--- trunk/CruceDeSemaforos/src/model/Auto.java
+++ trunk/CruceDeSemaforos/src/model/Auto.java
@@ -2,38 +2,77 @@ package model;
 
 import java.util.concurrent.Semaphore;
 
+public class Auto extends Thread {
 
-
-
-public abstract class Auto extends Thread{
-	
 	public int velocidad;
 	public Semaphore semaforoPaso1;
 	public String modeloAuto;
 	public String tipoDeCruce;
 	public Semaphore estoyPrimero;
 	public Semaforo semaforoDeMiCalle;
+	public int id;
 	public String direccion;
+	public static int fabricaId = 0;
 	public String calle;
-	
-	public Auto(int velocidad,  Semaphore semaforoCubito, String modelo, String tipoDeCruce, Semaphore estoyPrimero, Semaforo semaforoDeMiCalle,
-			 String direccion, String calle) {
-		
+
+	/**
+	 * Constructor
+	 * 
+	 * @param velocidad
+	 * @param calle
+	 * @param semaforoCubito
+	 * @param modelo
+	 * @param tipoDeCruce
+	 * @param estoyPrimero
+	 * @param semaforoDeMiCalle
+	 * @param direccion
+	 */
+	public Auto(int velocidad, Semaphore semaforoCubito,
+			String modelo, String tipoDeCruce, Semaphore estoyPrimero,
+			Semaforo semaforoDeMiCalle, String direccion, String calle, int id) {
+
 		this.velocidad = velocidad;
-		this.semaforoPaso1 =  semaforoCubito;
+		this.semaforoPaso1 = semaforoCubito;
 		this.modeloAuto = modelo;
 		this.tipoDeCruce = tipoDeCruce;
 		this.estoyPrimero = estoyPrimero;
 		this.semaforoDeMiCalle = semaforoDeMiCalle;
+        this.calle = calle;
 		this.direccion = direccion;
-		this.calle = calle;
+		this.id = id;
 	}
-	
-	
-	public void run(){}
-	
-	public void unirmeAlTransito(){
-		System.out.println(this.modeloAuto+" " +"  estoy en transito " + " calle " + this.calle);
+
+
+	/**
+	 * metodo run del thread, esta plasmado todo el comportamiento de un auto,
+	 * desde que entra en circulacion hasta que sale de ella.
+	 */
+	public void run() {
 		
+	}
+
+	
+	
+	
+
+	/**
+	 * Hace que el auto se inicie en el transito, solamente con una impresion.
+	 */
+	public synchronized void unirmeAlTransito() {
+		this.imprimirInformacion("  estoy en transito ");
+
+	}
+
+	
+
+	/**
+	 * Este metodo permite imporimer la informacion del auto, con alguna
+	 * variante mas que es el String que se pasa como parametro
+	 * 
+	 * @param info
+	 */
+	public void imprimirInformacion(String info) {
+		System.out.println(this.id + " " + this.modeloAuto + " " + info
+				+ " calle " + this.calle+ " " + this.direccion);
 	}
 }
--- trunk/CruceDeSemaforos/src/model/AutoCruceDoble.java
+++ trunk/CruceDeSemaforos/src/model/AutoCruceDoble.java
@@ -3,61 +3,78 @@ package model;
 import java.util.concurrent.Semaphore;
 
 public class AutoCruceDoble extends Auto {
-	
+
 	public Semaphore semaforoPaso2;
 	public String tipoDeCruce2;
 
-	public AutoCruceDoble(int velocidad,  Semaphore semaforoCubito, String modelo, String tipoDeCruce , 
-			Semaphore semaforoCubito2, String tipoDeCruce2, Semaphore esVerde, Semaforo semaforoDeMiCalle,  String direccion, String calle) {
-		super(velocidad,  semaforoCubito,  modelo,  tipoDeCruce, esVerde, semaforoDeMiCalle,    direccion, calle);
+	/**
+	 * Constructor del cual hereda parte de la superclase Auto
+	 * 
+	 * @param velocidad
+	 * @param calle
+	 * @param semaforoCubito
+	 * @param modelo
+	 * @param tipoDeCruce
+	 * @param semaforoCubito2
+	 * @param tipoDeCruce2
+	 * @param esVerde
+	 * @param semaforoDeMiCalle
+	 * @param direccion
+	 */
+	public AutoCruceDoble(int velocidad,
+			Semaphore semaforoCubito, String modelo, String tipoDeCruce,
+			Semaphore semaforoCubito2, String tipoDeCruce2, Semaphore esVerde,
+			Semaforo semaforoDeMiCalle, String direccion, String calle, int id) {
+		super(velocidad, semaforoCubito, modelo, tipoDeCruce, esVerde,
+				semaforoDeMiCalle, direccion, calle, id);
 		this.semaforoPaso2 = semaforoCubito2;
 		this.tipoDeCruce2 = tipoDeCruce2;
-		
-		
+
 	}
-	
-	@Override
+
 	public void run(){
 		try {
 			this.unirmeAlTransito();
 			
 			
-			
 			this.estoyPrimero.acquireUninterruptibly();
-			
-			System.out.println(this.modeloAuto+" "+"  estoy en transito, espero semaforo verde " + " calle " + this.calle+" "+this.direccion);
-			
-			if (! (this.semaforoDeMiCalle.getEstaEnVerde())) {
+
+			this.imprimirInformacion("  estoy en transito, espero semaforo verde ");
+
+			if (!(this.semaforoDeMiCalle.getEstaEnVerde())) {
 				this.semaforoDeMiCalle.getEsVerde().acquireUninterruptibly();
 			}
-			
-			
-				
-				
-				System.out.println(this.modeloAuto+" "+"  cruce semaforo "+ " calle " + this.calle+" "+this.direccion);
-				
-				this.semaforoPaso1.acquireUninterruptibly();
-				System.out.println(this.modeloAuto+" " +"  ocupe " + this.tipoDeCruce+" "+this.direccion);
-				Thread.sleep(this.velocidad);
 
-				this.estoyPrimero.release();
-				
-                System.out.println(this.modeloAuto+" " +"  desocupe " + this.tipoDeCruce+" "+this.direccion);
-				this.semaforoPaso2.acquireUninterruptibly();
-				this.semaforoPaso1.release();
-				
-				System.out.println(this.modeloAuto+" " +"  ocupe " + this.tipoDeCruce2+" "+this.direccion);
-				Thread.sleep(this.velocidad);
-				this.semaforoPaso2.release();
-				System.out.println(this.modeloAuto+" " +"  desocupe " + this.tipoDeCruce2+" "+this.direccion);
-				
-				System.out.println(this.modeloAuto+" "+"  abandone el cruce ");
-				
-			} catch (Exception e) {}
-		
+			this.imprimirInformacion("  cruce semaforo ");
+
+			
+			this.semaforoPaso1.acquireUninterruptibly();
+			System.out.println(this.id + " " + this.modeloAuto + " "
+					+ "  ocupe " + this.tipoDeCruce + " " + this.direccion);
+
+			Thread.sleep(this.velocidad);
+			this.estoyPrimero.release();
+			
+			this.imprimirInformacion("  desocupe ");
+			
+			this.semaforoPaso2.acquireUninterruptibly();
+			
+			this.semaforoPaso1.release();
+
+
+			this.imprimirInformacion("  ocupe ");
+
+			Thread.sleep(this.velocidad);
+
+			this.imprimirInformacion("  desocupe ");
+
+			System.out.println(this.id + " " + this.modeloAuto + " "
+					+ "  abandone el cruce ");
+			
+			this.semaforoPaso2.release();
+
+		} catch (Exception e) {
+		}
 	}
-	
-	
-	
 
 }
